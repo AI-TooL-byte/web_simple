42d74903e5c35c708243e237faafb244ddd412f2
YustePlaiaundi <ikeat@plaiaundi.net>
2026-09-23 08:49:15 +0200

    Inicio del proyecto web de anime

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+
+btn1.addEventListener('click', () => {
+    alert('Button 1 clicked!');
+});
+
+btn2.addEventListener('click', () => {
+    alert('Button 2 clicked!');
+});
